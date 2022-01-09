6972fe9c4dd34230a28bcbb6832602fb6a3b26e6
duc-ke <applekey2722@naver.com>
2022-01-09 23:11:30 +0900

    requirement 업데이트

---
+++ 1.annoying_face/requirements.sh
@@ -0,0 +1,11 @@
+# env(libs) setting
+pip install opencv-python numpy imutils cmake
+conda install -c conda-forge dlib
+
+# download dlib-model(68 face landmarks model)
+wget https://github.com/davisking/dlib-models/raw/master/shape_predictor_68_face_landmarks.dat.bz2
+
+# unzip bz2 (for mac or linux)
+# bunzip2 shape_predictor_68_face_landmarks.dat.bz2
+
+
