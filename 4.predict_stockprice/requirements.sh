3635384dfc16bac7c6f03f8a044f1e73546845e1
duc-ke <applekey2722@naver.com>
2022-02-08 21:23:11 +0900

    pytorch 버전 업로드

--- 4.predict_stockprice/requirements.sh
+++ 4.predict_stockprice/requirements.sh
@@ -1,4 +1,6 @@
-# env(libs) setting
+## env(libs) setting
 pip install tensorflow numpy matplotlib pandas
 conda install jupyter -y
+## pytorch setting 
+# pip install install torch torchvision torchaudio
 
